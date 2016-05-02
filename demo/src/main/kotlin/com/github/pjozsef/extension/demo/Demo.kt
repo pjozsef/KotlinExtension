958608aed4a72f8b98dfa3a5a203e9223cbe47c9
pjozsef <pollak.jozsef1@gmail.com>
2016-05-02 22:11:05 +0200

    Switched to more meaningful names

--- demo/src/main/kotlin/com/github/pjozsef/extension/demo/Demo.kt
+++ demo/src/main/kotlin/com/github/pjozsef/extension/demo/Demo.kt
@@ -7,33 +7,33 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 
-fun booleanDemo(dummyBoolean: Boolean, trueTask: () -> Unit, falseTask: () -> Unit) {
-    dummyBoolean {
-        trueTask()
+fun booleanDemo(result: Boolean) {
+    result {
+        print("yay")
     } otherwise {
-        falseTask()
+        print("meh")
     }
 
     //instead of
-    if (dummyBoolean) {
-        trueTask()
+    if (result) {
+        print("yay")
     } else {
-        falseTask()
+        print("meh")
     }
 }
 
-fun integerDemo(number: Int){
-    number.times{
+fun integerDemo(number: Int) {
+    number.times {
         println("Counting: $it")
     }
 
     //instead of
-    for(i in 0..number-1){
+    for (i in 0..number - 1) {
         println("Counting: $i")
     }
 }
 
-fun longAsCalendar_Or_AsDateDemo(long: Long){
+fun longAsCalendar_Or_AsDateDemo(long: Long) {
     val cal = long.asCalendar()
     val date = long.asDate()
 
@@ -44,7 +44,7 @@ fun longAsCalendar_Or_AsDateDemo(long: Long){
     val date2 = Date(long)
 }
 
-fun longDateFormatDemo(long: Long, customFormatString: String){
+fun longDateFormatDemo(long: Long, customFormatString: String) {
     val custom: String = long.format(customFormatString)
     val dateTime: String = long.formatAsDateTime()
     val date: String = long.formatAsDate()
