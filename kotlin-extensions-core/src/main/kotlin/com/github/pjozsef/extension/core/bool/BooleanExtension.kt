a05714a1b5f56bb62792ad7d5699567e2a0e004f
pjozsef <pollak.jozsef1@gmail.com>
2016-05-15 22:10:13 +0200

    Made extensions with lambdas inline

--- kotlin-extensions-core/src/main/kotlin/com/github/pjozsef/extension/core/bool/BooleanExtension.kt
+++ kotlin-extensions-core/src/main/kotlin/com/github/pjozsef/extension/core/bool/BooleanExtension.kt
@@ -1,27 +1,27 @@
 package com.github.pjozsef.extension.core.bool
 
-operator fun Boolean.invoke(action: () -> Unit): Boolean{
+inline operator fun Boolean.invoke(action: () -> Unit): Boolean{
     if(this){
         action()
     }
     return this
 }
 
-infix fun Boolean.otherwise(action: () -> Unit): Boolean{
+inline infix fun Boolean.otherwise(action: () -> Unit): Boolean{
     if(!this) {
         action()
     }
     return this
 }
 
-infix fun Boolean.then(action: ()->Unit): Boolean{
+inline infix fun Boolean.then(action: ()->Unit): Boolean{
     if(this){
         action()
     }
     return this
 }
 
-fun ifNot(bool: Boolean, action: ()->Unit){
+inline fun ifNot(bool: Boolean, action: ()->Unit){
     if(!bool){
         action()
     }
--- kotlin-extensions-core/src/main/kotlin/com/github/pjozsef/extension/core/integer/IntegerExtension.kt
+++ kotlin-extensions-core/src/main/kotlin/com/github/pjozsef/extension/core/integer/IntegerExtension.kt
@@ -1,6 +1,6 @@
 package com.github.pjozsef.extension.core.integer
 
-infix fun Int.times(action: (index: Int) -> Unit){
+inline infix fun Int.times(action: (index: Int) -> Unit){
     for(i in 1..this){
         action(i)
     }
